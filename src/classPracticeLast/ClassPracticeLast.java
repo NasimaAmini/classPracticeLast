5adfdf03be8614359070b1aeace14eabf4c00d49
nasim <nasim@Nasima>
2023-04-17 13:12:56 -0400

    Initial part

---
+++ src/classPracticeLast/ClassPracticeLast.java
@@ -0,0 +1,15 @@
+package classPracticeLast;
+
+public class ClassPracticeLast {
+	public static void main(String[] args) {
+		
+		System.out.println("nnnnnnnnnnnnnnnnnn");
+		
+		System.out.println("Push new cods");
+		
+		
+		System.out.println("Push new cods");
+
+	}
+
+}
